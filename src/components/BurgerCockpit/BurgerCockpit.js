3347ce9cac5095eee6bd77d7b56a75cc221781a9
Pablo Villacanas <pablo.villacanas@gmail.com>
2019-11-05 19:21:02 +0100

    Fixed Burger cockpit css

--- src/components/BurgerCockpit/BurgerCockpit.js
+++ src/components/BurgerCockpit/BurgerCockpit.js
@@ -22,22 +22,22 @@ const BurgerCockpit = (props) => {
 
 	return (
 		<div className="burgerCockpit">
-			<div className="ingredients">
-				{
-					controls.map(ctrl => (
-						<IngredientControl
-							ingredients={props.ingredients}
-							onAdd={() => props.onAddIngredient(ctrl.type)}
-							onRemove={() => props.onRemoveIngredient(ctrl.type)}
-							key={ctrl.type} label={ctrl.label} type={ctrl.type} />
-					))
-				}
+			<div className="orderPanel">
+				<div className="ingredients">
+					{
+						controls.map(ctrl => (
+							<IngredientControl
+								ingredients={props.ingredients}
+								onAdd={() => props.onAddIngredient(ctrl.type)}
+								onRemove={() => props.onRemoveIngredient(ctrl.type)}
+								key={ctrl.type} label={ctrl.label} type={ctrl.type} />
+						))
+					}
+				</div>
+				<button ref={orderButton} onClick={props.onCheckout} className="orderButton">Order now!</button>
 			</div>
 			<div className="totalPrice">
 				Total : {props.totalPrice} €
-			</div>
-			<div className="orderPanel">
-				<button ref={orderButton} onClick={props.onCheckout} className="orderButton">Order now!</button>
 			</div>
 		</div>
 	)
